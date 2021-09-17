6c33cc8eb274da79d6ffe4c05b4a97ef4d596005
Johnsan0327 <johnsan2k@gmail.com>
2021-09-17 07:34:50 +0530

    reverting the last commit

--- src/app/utils/field-validation/validators/districtValidator.ts
+++ src/app/utils/field-validation/validators/districtValidator.ts
@@ -1,45 +1,48 @@
 import { chain, left, right } from 'fp-ts/lib/Either'
 import { flow } from 'fp-ts/lib/function'
 
-import { IDistrictFieldSchema } from '../../../../types/field/districtField'
+import {
+  IDistrictFieldSchema,
+  OmitUnusedValidatorProps,
+} from '../../../../types/field'
 import { ResponseValidator } from '../../../../types/field/utils/validation'
+import districtField from '../../../models/field/districtField'
 import { getMyInfoFieldOptions } from '../../../modules/myinfo/myinfo.util'
 import { ProcessedSingleAnswerResponse } from '../../../modules/submission/submission.types'
 
 import { notEmptySingleAnswerResponse } from './common'
 import { isOneOfOptions } from './options'
-import {OmitUnusedValidatorProps} from "../../../../types";
 
 type DistrictValidator = ResponseValidator<ProcessedSingleAnswerResponse>
 type DistrictValidatorConstructor = (
-    districtField: OmitUnusedValidatorProps<IDistrictFieldSchema>,
+  districtField: OmitUnusedValidatorProps<IDistrictFieldSchema>,
 ) => DistrictValidator
 
 /**
  * Returns a validation function
- * to check if district selection is one of the options.
+ * to check if dropdown selection is one of the options.
  */
 const makeDistrictValidator: DistrictValidatorConstructor =
-    (districtField) => (response) => {
-        const { myInfo, fieldOptions } = districtField
-        // Inject fieldOptions for MyInfo. This is necessary because the
-        // client strips out MyInfo data to keep each form submission lightweight
-        const validOptions = myInfo?.attr
-            ? getMyInfoFieldOptions(myInfo.attr)
-            : fieldOptions
-        const { answer } = response
-        return isOneOfOptions(validOptions, answer)
-            ? right(response)
-            : left(`DistrictValidator:\t answer is not a valid district option`)
-    }
+  (districtField) => (response) => {
+    const { myInfo, fieldOptions } = districtField
+    // Inject fieldOptions for MyInfo. This is necessary because the
+    // client strips out MyInfo data to keep each form submission lightweight
+    const validOptions = myInfo?.attr
+      ? getMyInfoFieldOptions(myInfo.attr)
+      : fieldOptions
+    const { answer } = response
+    return isOneOfOptions(validOptions, answer)
+      ? right(response)
+      : left(`DropdownValidator:\t answer is not a valid dropdown option`)
+  }
 
 /**
- * Returns a validation function for a district field when called.
+ * Returns a validation function for a dropdown field when called.
  */
 export const constructDistrictValidator: DistrictValidatorConstructor = (
-    districtField,
+  districtField,
 ) =>
-    flow(
-        notEmptySingleAnswerResponse,
-        chain(makeDistrictValidator(districtField)),
-    )
+  flow(
+    notEmptySingleAnswerResponse,
+    chain(makeDistrictValidator(districtField)),
+  )
